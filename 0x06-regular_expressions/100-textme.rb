e32ef26d7f9fa223d24db88ebde3c01a90468999
Andres802 <reachandres@gmail.com>
2020-11-11 01:30:12 +0000

    she bang fixed

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,2 @@
-#/usr/bin/env ruby
-#puts ARGV[0].scan(/\[[f|t]\w{1,5}:[\w\\+-]+:?\d?:?-?\d?:?\d?:?-?\d?\]/).join(",")
-puts ARGV[0].scan(/(?<=from:|to:|flags:)[\-+\d\w:]*/).join(",")
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/(?<=from:|to:|flags:).*?(?=\])/).join(",")
